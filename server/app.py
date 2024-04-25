289cb351ddb92043607a9a7fbe85c22fdcae4ba2
Brandi Burnell <brandiburnell6@gmail.com>
2024-04-24 19:16:25 -0600

    pass tests

--- server/app.py
+++ server/app.py
@@ -28,16 +28,34 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
 
+        session['user_id'] = user.id
+
+        return user.to_dict()
+                                 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        session['user_id'] = None
+        return {}, 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
